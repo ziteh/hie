d3ce7e87b1b0830d604405153b8255860c271a1f
ziteh <honmonoh@gmail.com>
2024-08-24 15:18:03 +0800

    feat: override Swagger UI background color

--- app/api/docs/page.tsx
+++ app/api/docs/page.tsx
@@ -1,5 +1,6 @@
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
+import "./custom.css";
 
 export default function ApiDocs() {
   return <SwaggerUI url="/open-api/document.yaml" />;
