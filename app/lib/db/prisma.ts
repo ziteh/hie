9f30f252ca9fc839a36138e9cf9ae26672c7a6ce
ziteh <honmonoh@gmail.com>
2024-09-15 16:39:25 +0800

    chore: change logging level of Prisma

--- app/lib/db/prisma.ts
+++ app/lib/db/prisma.ts
@@ -5,7 +5,7 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ["query"],
+    log: ["info", "warn", "error"], // Logging level
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
