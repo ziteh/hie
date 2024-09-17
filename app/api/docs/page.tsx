e45e57ee4ffb7c6318e8dfbecea6e5b5891f9a88
ziteh <honmonoh@gmail.com>
2024-09-17 16:40:26 +0800

    refactor: rename open-api folder to openapi

--- app/api/docs/page.tsx
+++ app/api/docs/page.tsx
@@ -5,5 +5,5 @@ import "swagger-ui-react/swagger-ui.css";
 import "./custom.css";
 
 export default function ApiDocs() {
-  return <SwaggerUI url="/open-api/document.yaml" />;
+  return <SwaggerUI url="/openapi/document.yaml" />;
 }
