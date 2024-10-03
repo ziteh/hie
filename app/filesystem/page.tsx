7f06a701e196e43e0386b0f09e185e754e26516a
ziteh <honmonoh@gmail.com>
2024-10-03 19:43:37 +0800

    feat: add folder navigation for filesystem page

--- app/filesystem/page.tsx
+++ app/filesystem/page.tsx
@@ -23,6 +23,7 @@ export default function Page() {
   const [selectedFolder, setSelectedFolder] = useState<Folder | null>(null);
   const [fileStructureSet, setFileStructureSet] =
     useState<FileStructure | null>(null);
+  const [subFolders, setSubFolders] = useState<string[]>([]);
 
   const router = useRouter();
 
@@ -48,8 +49,34 @@ export default function Page() {
     setFileStructureSet(fs);
   };
 
+  const handleSubFolderAdd = async (newFolder: string) => {
+    handlePathChange(subFolders.concat(newFolder));
+  };
+
+  const handleSubFolderRemove = async () => {
+    if (subFolders.length === 0) {
+      return;
+    }
+    handlePathChange(subFolders.slice(0, -1));
+  };
+
+  const handlePathChange = async (subFolders: string[]) => {
+    if (selectedFolder === null) {
+      return;
+    }
+
+    const path = subFolders.join("/");
+    const fullPath = selectedFolder.path + path;
+    const fs = await getFileStructure(fullPath);
+    setFileStructureSet(fs);
+
+    setSubFolders(subFolders);
+  };
+
   return (
     <div>
+      <Button onClick={() => handleSubFolderRemove()}>Back</Button>
+
       <Select label="Folder" onChange={handleSelectFolder}>
         {folders.map((folder: Folder) => (
           <MenuItem key={folder.id} value={folder.id}>
@@ -57,9 +84,12 @@ export default function Page() {
           </MenuItem>
         ))}
       </Select>
+      {fileStructureSet?.dir.map((d) => (
+        <Button key={d} onClick={() => handleSubFolderAdd(d)}>
+          {d}
+        </Button>
+      ))}
       {fileStructureSet?.files.map((file) => <p key={file}>{file}</p>)}
-      <p>DIR=================</p>
-      {fileStructureSet?.dir.map((file) => <p key={file}>{file}</p>)}
     </div>
   );
 }
